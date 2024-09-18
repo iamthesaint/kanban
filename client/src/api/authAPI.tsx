c3d730e26fc9aba58033b5a570b6c03f4b7b85bb
iamthesaint <stephenie2@me.com>
2024-09-18 18:36:35 -0400

    updated

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -1,9 +1,25 @@
 import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
-  // TODO: make a POST request to the login route
+// make a POST request to the login route
+  try {
+    const response = await fetch("/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userInfo),
+    });
+
+      const data = await response.json();
+      if (!response.ok) {
+      throw new Error("Failed to login");
+    }
+    return data;
+  } catch (error) {
+    console.log('error', error);
+    return Promise.reject('Failed to login');
 }
-
-
+};
 
 export { login };
