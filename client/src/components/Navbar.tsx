e95c24d0d60d0df938c13c0f9f7bb751af33183d
iamthesaint <stephenie2@me.com>
2024-09-18 21:47:52 -0400

    added functionality to direct to login page when unauthenticated users click kanban button

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import auth from "../utils/auth";
+
 
 const Navbar = () => {
   const [loginCheck, setLoginCheck] = useState(false);
-
+  const navigate = useNavigate();
   const checkLogin = () => {
     if (auth.loggedIn()) {
       setLoginCheck(true);
@@ -16,10 +17,18 @@ const Navbar = () => {
     checkLogin();
   }, [loginCheck]);
 
+  const handleKanbanClick = () => {
+    if (!loginCheck) {
+      navigate("/login");
+    } else {
+      navigate("/");
+    }
+  }
+
   return (
     <div className="nav">
       <div className="nav-title">
-        <Link to="/">Krazy Kanban Board</Link>
+        <Link to="#" onClick={handleKanbanClick}>Krazy Kanban Board</Link>
       </div>
       <ul>
         {!loginCheck ? (
