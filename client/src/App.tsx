c7469e26d0565b4a6455d5940934c16ef31ad82f
iamthesaint <stephenie2@me.com>
2024-09-19 09:42:55 -0400

    removed consoles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,21 +3,14 @@ import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 
 function App() {
-  console.log('Environment Variables:');
-  console.log('REACT_APP_API_URL:', process.env.REACT_APP_API_URL);
 
   return (
-    <>
-      <div>
-        <h1>ENV var check</h1>
-      </div>
-      <div className='container'>
-        <Navbar />
-        <main>
-          <Outlet />
-        </main>
-      </div>
-    </>
+    <div className='container'>
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
+    </div>
   )
 }
 
